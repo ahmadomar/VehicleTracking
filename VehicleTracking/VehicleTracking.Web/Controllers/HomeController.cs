74f0b2961a112b6bedee5cd6c45dfd27114c4e86
Ahmed Omar <dev.ahmedomar@gmail.com>
2019-04-26 05:07:34 +0200

    Delete unused code

--- VehicleTracking/VehicleTracking.Web/Controllers/HomeController.cs
+++ VehicleTracking/VehicleTracking.Web/Controllers/HomeController.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.SignalR.Client;
-using Microsoft.AspNetCore.Mvc;
 using VehicleTracking.Models;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace VehicleTracking.Web.Controllers
 {
@@ -18,42 +12,5 @@ namespace VehicleTracking.Web.Controllers
             var vehiclesList = data.SelectMany(v => v.Vehicles.ToList()).ToList();
             return View(vehiclesList);
         }
-    }
-
-
-
-    public class SignalRMasterClient
-    {
-        public string Url { get; set; }
-        public HubConnection Connection { get; set; }
-        public IHubProxy Hub { get; set; }
-
-        public SignalRMasterClient(string url)
-        {
-            Url = url;
-            Connection = new HubConnection(url, useDefaultUrl: false);
-            Hub = Connection.CreateHubProxy("VehicleStatusHub");
-            Connection.Start().Wait();
-
-            Hub.On<string>("acknowledgeMessage", (message) =>
-            {
-                Console.WriteLine("Message received: " + message);
-
-                /// TODO: Check status of the LDAP
-                /// and update status to Web API.
-            });
-        }
-
-        public void SayHello(string message)
-        {
-            Hub.Invoke("hello", message);
-            Console.WriteLine("hello method is called!");
-        }
-
-        public void Stop()
-        {
-            Connection.Stop();
-        }
-
     }
 }
