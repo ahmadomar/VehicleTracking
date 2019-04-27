2711e310a50e4ba29f1bd35db78a1e114f86547c
Ahmed Omar <dev.ahmedomar@gmail.com>
2019-04-27 15:31:05 +0200

    Filter Changes

--- VehicleTracking/VehicleTracking.API/Controllers/FilterController.cs
+++ VehicleTracking/VehicleTracking.API/Controllers/FilterController.cs
@@ -1,12 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using RawRabbit;
 using RestSharp;
 using System.Collections.Generic;
-using System.Threading.Tasks;
 using VehicleTracking.API.Hubs;
-using VehicleTracking.Common.MQ.Commands;
 using VehicleTracking.Models;
 
 namespace VehicleTracking.API.Controllers
@@ -16,19 +15,23 @@ namespace VehicleTracking.API.Controllers
     {
         private readonly IBusClient _busClient;
         private readonly IHubContext<VehicleHub> _vehHub;
-        public FilterController(IBusClient busClient, IHubContext<VehicleHub> vehHub)
+        private readonly IConfiguration _iConfig;
+        public FilterController(IBusClient busClient, IHubContext<VehicleHub> vehHub,
+            IConfiguration iConfig)
 
         {
             _busClient = busClient;
             _vehHub = vehHub;
+            _iConfig = iConfig;
         }
 
 
         [HttpGet]
         public IEnumerable<VehicleModel> Get(string filter, string status)
         {
+            var filterApi = _iConfig.GetValue<string>("DataMS-Filter");
 
-            var client = new RestClient("https://localhost:5050/api/Filter");
+            var client = new RestClient(filterApi);
             var request = new RestRequest(Method.GET);
             request.AddParameter("filter", filter);
             request.AddParameter("status", status);
--- VehicleTracking/VehicleTracking.Web/Controllers/HomeController.cs
+++ VehicleTracking/VehicleTracking.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using RestSharp;
 using System.Collections.Generic;
@@ -8,28 +9,48 @@ namespace VehicleTracking.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IConfiguration _iConfig;
+        public HomeController(IConfiguration iConfig)
+        {
+            _iConfig = iConfig;
+        }
+
         public IActionResult Index()
         {
             var vehiclesList = ReadVehicles();
+            if (vehiclesList == null)
+                vehiclesList = new List<VehicleModel>();
             return View(vehiclesList);
         }
 
         [HttpGet]
         public IActionResult Filter(string filter= null, string status = null)
         {
-            var results = ReadVehicles(filter, status);
-            return PartialView("_FilterResultView", results);
+            var vehiclesList = ReadVehicles(filter, status);
+            if (vehiclesList == null)
+                vehiclesList = new List<VehicleModel>();
+
+            return PartialView("_FilterResultView", vehiclesList);
         }
 
         private List<VehicleModel> ReadVehicles(string filter = null, string status = null)
         {
-            var client = new RestClient("https://localhost:5001/Filter");
-            var request = new RestRequest(Method.GET);
-            request.AddParameter("filter", filter);
-            request.AddParameter("status", status);
+            try
+            {
+                var filterApi = _iConfig.GetValue<string>("GatewayAPI-Filter");
 
-            IRestResponse response = client.Execute(request);
-            return JsonConvert.DeserializeObject<List<VehicleModel>>(response.Content);
+                var client = new RestClient(filterApi);
+                var request = new RestRequest(Method.GET);
+                request.AddParameter("filter", filter);
+                request.AddParameter("status", status);
+
+                IRestResponse response = client.Execute(request);
+                return JsonConvert.DeserializeObject<List<VehicleModel>>(response.Content);
+            }
+            catch
+            {
+                return new List<VehicleModel>();
+            }
         }
     }
 }
