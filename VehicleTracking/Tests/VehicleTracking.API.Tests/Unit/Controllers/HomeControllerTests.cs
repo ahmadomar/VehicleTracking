f62be2f8fcdbd3ba299e1531f1ec776a7757cede
Ahmed Omar <ahmad3omar@outlook.com>
2019-04-30 21:44:01 +0200

    Adding xUnit Projects

---
+++ VehicleTracking/Tests/VehicleTracking.API.Tests/Unit/Controllers/HomeControllerTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VehicleTracking.API.Tests.Unit.Controllers
+{
+    public class HomeControllerTests
+    {
+        public void Home_Controller_Get_Should_return_strint_content()
+        {
+
+        }
+    }
+}
