6384dca38089262e10b9b04362355f3e3900205a
Ahmed Omar <ahmad3omar@outlook.com>
2019-05-02 10:56:47 +0200

    Use external request class to manipulate the data

--- VehicleTracking/VehicleTracking.API/Controllers/FilterController.cs
+++ VehicleTracking/VehicleTracking.API/Controllers/FilterController.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SignalR;
-using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json;
-using RawRabbit;
-using RestSharp;
 using System.Collections.Generic;
-using VehicleTracking.API.Hubs;
+using VehicleTracking.API.Helpers;
 using VehicleTracking.Models;
 
 namespace VehicleTracking.API.Controllers
@@ -13,31 +8,17 @@ namespace VehicleTracking.API.Controllers
     [Route("[controller]")]
     public class FilterController : Controller
     {
-        private readonly IBusClient _busClient;
-        private readonly IHubContext<VehicleHub> _vehHub;
-        private readonly IConfiguration _iConfig;
-        public FilterController(IBusClient busClient, IHubContext<VehicleHub> vehHub,
-            IConfiguration iConfig)
-
+        private readonly IExternalRequest _externalRequest;
+        public FilterController(IExternalRequest externalRequest)
         {
-            _busClient = busClient;
-            _vehHub = vehHub;
-            _iConfig = iConfig;
+            _externalRequest = externalRequest;
         }
 
 
         [HttpGet]
         public IEnumerable<VehicleViewModel> Get(string filter, string status)
         {
-            var filterApi = _iConfig.GetValue<string>("DataMS-Filter");
-
-            var client = new RestClient(filterApi);
-            var request = new RestRequest(Method.GET);
-            request.AddParameter("filter", filter);
-            request.AddParameter("status", status);
-
-            IRestResponse response = client.Execute(request);
-            return JsonConvert.DeserializeObject<List<VehicleViewModel>>(response.Content);
+            return _externalRequest.ReadAllVehicles(filter, status);
         }
     }
 }
---
+++ VehicleTracking/VehicleTracking.API/Helpers/ExternalRequest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Collections.Generic;
+using VehicleTracking.Models;
+
+namespace VehicleTracking.API.Helpers
+{
+    public interface IExternalRequest
+    {
+        List<VehicleViewModel> ReadAllVehicles(string filter = null, string status = null);
+    }
+
+    public class ExternalRequest : IExternalRequest
+    {
+        private readonly IConfiguration _configuration;
+        public ExternalRequest(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public List<VehicleViewModel> ReadAllVehicles(string filter = null, string status = null)
+        {
+            var filterApi = _configuration.GetValue<string>("DataMS-Filter");
+
+            var client = new RestClient(filterApi);
+            var request = new RestRequest(Method.GET);
+            request.AddParameter("filter", filter);
+            request.AddParameter("status", status);
+
+            IRestResponse response = client.Execute(request);
+            return JsonConvert.DeserializeObject<List<VehicleViewModel>>(response.Content);
+        }
+    }
+}
